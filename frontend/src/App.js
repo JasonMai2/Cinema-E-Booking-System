4e59387fb1cab0f696ca4e8c4633c55422bc2098
Kimmygatez <132913098+Kimmygatez@users.noreply.github.com>
2025-09-30 16:43:17 -0400

    Add preliminary edit profile page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Header from './components/Header';
 import Login from './pages/Login';
+import EditProfile from './pages/EditProfile';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/profile/edit" element={<EditProfile />} />
       </Routes>
     </Router>
   );
