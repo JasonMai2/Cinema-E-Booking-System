27b416fef8e69c1b994be2f1b7cc9ce5cc56f064
evanf01 <45178602+evanf01@users.noreply.github.com>
2025-10-04 13:48:54 -0400

    feat(checkout): add Checkout page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Header from './components/Header';
+import ShowTimes from './pages/ShowTimes';
+import SeatSelection from './pages/SeatSelection';
+import Checkout from './pages/Checkout';
+import OrderSummary from './pages/OrderSummary';
+import OrderConfirmation from './pages/OrderConfirmation';
+import { BookingProvider } from './context/BookingContext';
 
 function App() {
   return (
-    <Router>
-      <Header /> 
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <BookingProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shows" element={<ShowTimes />} />
+          <Route path="/shows/:showId/seats" element={<SeatSelection />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/order-summary" element={<OrderSummary />} />
+          <Route path="/order-confirmation/:orderId" element={<OrderConfirmation />} />
+        </Routes>
+      </Router>
+    </BookingProvider>
   );
 }
 
