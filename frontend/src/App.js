180967669a29520884598caef1bea64e799fb88c
JasonMai2 <jasonmai0405@yahoo.com>
2025-10-05 20:22:34 -0400

    AdminDashboard page layout created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Header from './components/Header';
+import AdminDashboard from "./pages/AdminDashboard";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header /> 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<AdminDashboard />} />
       </Routes>
     </Router>
   );
