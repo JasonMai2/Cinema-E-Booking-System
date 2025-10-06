e8dc06508f47122fdbc81a44f27bd8c1deda6bc2
Aryan Patel <aryanpatel109@icloud.com>
2025-10-05 23:42:10 -0400

    Add movie seed and import script for team

---
+++ seeds_movies.sql
@@ -0,0 +1,12 @@
+-- seeds_movies.sql  -- minimal movie seed matching your movies table
+INSERT INTO movies (title, synopsis, mpaa_rating, trailer_video_url, trailer_image_url) VALUES
+('Inception', 'A thief who steals corporate secrets through dream-sharing technology.', 'PG-13', NULL, 'https://image.tmdb.org/t/p/w500/qmDpIHrmpJINaRKAfWQfftjCdyi.jpg'),
+('The Matrix', 'A hacker discovers reality is a simulation and joins a rebellion.', 'R', NULL, 'https://image.tmdb.org/t/p/w500/f89U3ADr1oiB1s9GkdPOEpXUk5H.jpg'),
+('The Shawshank Redemption', 'Two imprisoned men bond over a number of years.', 'R', NULL, 'https://image.tmdb.org/t/p/w500/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg'),
+('Interstellar', 'A team travels through a wormhole to save humanity.', 'PG-13', NULL, 'https://image.tmdb.org/t/p/w500/rAiYTfKGqDCRIIqo664sY9XZIvQ.jpg'),
+('Avengers: Endgame', 'Heroes assemble to undo Thanos'' actions.', 'PG-13', NULL, 'https://image.tmdb.org/t/p/w500/or06FN3Dka5tukK1e9sl16pB3iy.jpg'),
+('Titanic', 'A romance on the doomed ocean liner.', 'PG-13', NULL, 'https://image.tmdb.org/t/p/w500/kHXEpyfl6zqn8a6YuozZUujufXf.jpg'),
+('Jurassic Park', 'Dinosaurs are brought back to life with catastrophic results.', 'PG-13', NULL, 'https://image.tmdb.org/t/p/w500/c414cDeQ9b6qLPLeKmiJuLDUREJ.jpg'),
+('The Dark Knight', 'Batman faces the Joker in Gotham City.', 'PG-13', NULL, 'https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg'),
+('Pulp Fiction', 'Multiple intertwined stories from LA criminals.', 'R', NULL, 'https://image.tmdb.org/t/p/w500/dM2w364MScsjFf8pfMbaWUcWrR.jpg'),
+('The Social Network', 'The founding and rise of Facebook.', 'PG-13', NULL, 'https://image.tmdb.org/t/p/w500/9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg');
