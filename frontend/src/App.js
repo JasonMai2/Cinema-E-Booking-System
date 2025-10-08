a68d9745b997f79912ec9751ddb10d5a0345c6fc
evanf01 <45178602+evanf01@users.noreply.github.com>
2025-10-08 17:38:48 -0400

    resolve merge conflicts in App.js and combine routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-<<<<<<< HEAD
+import Header from './components/Header';
 import Home from './pages/Home';
 import ShowTimes from './pages/ShowTimes';
 import SeatSelection from './pages/SeatSelection';
@@ -8,9 +8,12 @@ import Checkout from './pages/Checkout';
 import OrderSummary from './pages/OrderSummary';
 import OrderConfirmation from './pages/OrderConfirmation';
 import { BookingProvider } from './context/BookingContext';
-import Header from './components/Header';
 import Login from './pages/Login';
 import MovieDetails from './pages/MovieDetails';
+import AdminDashboard from './pages/AdminDashboard';
+import RegistrationConfirmation from './pages/RegistrationConfirmation';
+import EditProfile from './pages/EditProfile';
+import MovieSelection from './pages/MovieSelection';
 
 function App() {
   return (
@@ -26,37 +29,15 @@ function App() {
           <Route path="/checkout" element={<Checkout />} />
           <Route path="/order-summary" element={<OrderSummary />} />
           <Route path="/order-confirmation/:orderId" element={<OrderConfirmation />} />
+
+          {/* Admin / additional pages */}
+          <Route path="/admin" element={<AdminDashboard />} />
+          <Route path="/registration-confirmation" element={<RegistrationConfirmation />} />
+          <Route path="/profile/edit" element={<EditProfile />} />
+          <Route path="/movies" element={<MovieSelection />} />
         </Routes>
       </Router>
     </BookingProvider>
-=======
-import Header from './components/Header';
-import AdminDashboard from "./pages/AdminDashboard";
-import Home from './pages/Home';
-import MovieDetails from "./pages/MovieDetails";
-import Login from "./pages/Login";
-import RegistrationConfirmation from "./pages/RegistrationConfirmation";
-import EditProfile from "./pages/EditProfile";
-import MovieSelection from './pages/MovieSelection';
-
-function App() {
-  return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/admin" element={<AdminDashboard />} />
-        <Route path="/movie-details" element={<MovieDetails />} />
-        <Route path="/login" element={<Login />} />
-        <Route
-          path="/registration-confirmation"
-          element={<RegistrationConfirmation />}
-        />
-        <Route path="/profile/edit" element={<EditProfile />} />
-        <Route path="/movies" element={<MovieSelection />} />
-      </Routes>
-    </Router>
->>>>>>> e8dc06508f47122fdbc81a44f27bd8c1deda6bc2
   );
 }
 
